447862ce6fe373d95b66bca62eddf145f7411488
rsip22 <rsip22@openmailbox.org>
2018-01-18 22:44:01 -0200

    Simple fetching JSON from Agenda Du Libre

---
+++ crawler.py
@@ -0,0 +1,20 @@
+import urllib.request
+import json
+
+
+def get_events_from_url(url):
+    source = urllib.request.urlopen(url)
+    data = source.read().decode()
+
+    events_list = json.loads(data)
+
+    # print(json.dumps(events_list, indent=2))
+
+    for event in events_list:
+        print('Event:', event['title'])
+        print('City:', event['city'])
+        print('Date:', event['start_time'], '-', event['end_time'])
+        print('==============================')
+
+
+get_events_from_url('http://www.agendadulibre.org/events.json?utf8=%E2%9C%93&near%5Blocation%5D=&near%5Bdistance%5D=&region=&tag=&button=')
